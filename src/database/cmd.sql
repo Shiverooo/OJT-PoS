f748f6e8d26a294640ff1d1bd13381879aec38fd
John Philip Barnachia <jpbarnachia1234@gmail.com>
2025-03-25 13:23:44 +0800

    changes to database

---
+++ src/database/cmd.sql
@@ -0,0 +1,54 @@
+-- CREATE TABLE users (
+--     user_id INTEGER UNIQUE,
+--     first_name VARCHAR(100) NOT NULL,
+--     last_name VARCHAR(100) NOT NULL,
+--     email VARCHAR(255) UNIQUE NOT NULL,
+--     phone_number VARCHAR(20),
+--     username VARCHAR(100) UNIQUE NOT NULL,
+--     password_hash TEXT NOT NULL,
+--     role TEXT NOT NULL CHECK (role IN ('admin', 'cashier')) DEFAULT 'cashier'
+-- );
+
+-- CREATE TRIGGER set_user_id
+-- AFTER INSERT ON users
+-- FOR EACH ROW
+-- WHEN NEW.user_id IS NULL
+-- BEGIN
+--     UPDATE users
+--     SET user_id = (abs(random()) % 1000 + 2025000)
+--     WHERE rowid = NEW.rowid;
+-- END;
+
+-- insert into users(
+-- first_name,
+-- last_name,
+-- email,
+-- phone_number,
+-- username,
+-- password_hash
+-- )
+-- values(
+-- 'Jerson',
+-- 'Mamangun',
+-- 'jeron.mamangun@gmail.com',
+-- '09123456789',
+-- 'Jeron',
+-- 'MyNewNameIsJeron'
+-- );
+
+SELECT * FROM users;
+
+-- DROP TABLE users;
+
+-- create table sample(
+--     id integer primary key default (random()),
+--     username varchar(100) unique not null
+-- );
+
+-- INSERT INTO sample(
+--     username
+-- )
+-- VALUES(
+--     'Jeron'
+-- );
+
