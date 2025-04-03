5dd853d421d0bbd2ee3b0df60460dfdbe7ba96f7
John Philip Barnachia <jpbarnachia1234@gmail.com>
2025-04-03 16:07:12 +0800

    added search to product management

--- src/page/admin-dashboard/dashboard/product-management.tsx
+++ src/page/admin-dashboard/dashboard/product-management.tsx
@@ -1,12 +1,37 @@
-import React from 'react'
-import '../../../styles/admin/product-management.css'
+import React, { useState } from 'react';
+import '../../../styles/admin/product-management.css'; 
+import searchIcon from "../../../assets/images/search-icon.svg"; 
 
+// Functional component for Product Management
 function ProductManagement() {
+  const [searchQuery, setSearchQuery] = useState(''); 
+
+  const handleInputChange = (e) => {
+    setSearchQuery(e.target.value);
+  };
+
   return (
-    <div>
-      Hello
+    <div className="management-container">
+      <div className="header-management">
+        <div className="search-prod-management">
+          <div className="search-bar-management">
+            <img 
+              src={searchIcon} 
+              alt="Search Icon" 
+              className="search-icon" 
+            />
+            <input
+              type="text"
+              placeholder="Search product, supplier, order"
+              className="prod-search-input"
+              value={searchQuery} 
+              onChange={handleInputChange} 
+            />
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default ProductManagement
+export default ProductManagement;
