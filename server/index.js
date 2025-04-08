fc9bea1b5266ec5a7c90f3f7306bacaddaeb88dd
John Philip Barnachia <jpbarnachia1234@gmail.com>
2025-04-08 09:56:07 +0800

    changes to file structure

---
+++ server/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const db = require('better-sqlite3')('./database/database.db', {verbose: console.log});
+const app = express();
+const port = 3000;
+const users = db.prepare('select * from users').all();
+
+app.get('/', (req,res)=>{
+    res.json({users:users})
+})
+
+app.get('/user', (req,res) =>{
+    res.send(users);
+})
+
+app.listen(port, ()=>{
+    console.log(`Viewing at port ${port}`);
+})
