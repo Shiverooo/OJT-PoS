be1c64807a3b480a5e8a5213052bfe4c9bac536f
John Philip Barnachia <jpbarnachia1234@gmail.com>
2025-04-08 10:25:54 +0800

    changes to file structure

--- client/src/database/sqlite.js
+++
@@ -1,9 +0,0 @@
-import Database from 'better-sqlite3';
-
-const db = new Database('./database.db', {verbose: console.log});
-db.pragma('journal_mode = WAL');
-
-const stmt = db.prepare('select * from users');
-const display = stmt.run()
-console.log(displaycd );
-
