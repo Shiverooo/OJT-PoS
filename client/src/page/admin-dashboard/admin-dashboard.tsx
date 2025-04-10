19cc15042dcbe3bc1447762a148e26d482571ee5
John Philip Barnachia <jpbarnachia1234@gmail.com>
2025-04-10 16:59:47 +0800

    .

--- client/src/page/admin-dashboard/admin-dashboard.tsx
+++ client/src/page/admin-dashboard/admin-dashboard.tsx
@@ -5,7 +5,7 @@ import logo from "../../assets/images/infinitum.png";
 import dashboardIcon from "../../assets/images/dashboard-icon.svg";
 import productIcon from "../../assets/images/barcode_icon.svg";
 import userIcon from "../../assets/images/user-management-icon.svg";
-import salesIcon from "../../assets/images/sale-reports-icon.svg";
+import salesIcon from "../../assets/images/sale-reports-icon.svg";  
 import menuIcon from "../../assets/images/menu-icon.svg";
 import signOutIcon from "../../assets/images/sign-out.svg";
 
@@ -79,6 +79,22 @@ function AdminDashboard() {
                   className="nav-icon"
                 />
                 Product Management
+              </li>
+            </Link>
+            <Link to="/admin/supplier">
+              <li
+                className={`admin-item ${
+                  location.pathname === "/admin/supplier"
+                    ? "active"
+                    : ""
+                }`}
+              >
+                <img
+                  src={productIcon}
+                  alt="Supplier Icon"
+                  className="nav-icon"
+                />
+                Supplier
               </li>
             </Link>
             <Link to="/admin/user-management">
---
+++ client/src/page/admin-dashboard/dashboard/supplier.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Supplier() {
+  return (
+    <div>
+        Very Good Jeron
+    </div>
+  )
+}
+
+export default Supplier;
