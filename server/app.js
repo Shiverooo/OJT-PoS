0a4831ac0bdd17caac54dc633ed115c9c0cf1b59
John Philip Barnachia <jpbarnachia1234@gmail.com>
2025-04-21 13:12:23 +0800

    created working hashed password on creation

--- server/app.js
+++ server/app.js
@@ -4,16 +4,15 @@ const getUserDB = require('./routes/userRoutes.js');
 const app = express();
 const loadMiddleware = require('./middleware/middleware.js');
 
-
-loadMiddleware(app);
-
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+loadMiddleware(app);
 
 /*For debugging*/
 app.use('/users', getUserDB)
 
-app.get('/', (req,res)=>{
-    res.redirect('/users')
-})
+// app.get('/', (req,res)=>{
+//     res.redirect('/users')
+// })
 
 module.exports = app;
--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,17 +1,18 @@
 const db = require('../models/sqliteModel.js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
 const SECRET_KEY = process.env.JWT_SECRET;
-
 
 exports.authLogin = (req, res) =>{
    const {email, password} = req.body;
-   const user = db.prepare('select id, email, password_hash, role from users where email = ? and password_hash = ?').get(email, password);
-   if(!user) return res.status(404).json({message:'User not found'});
+   const user = db.prepare('select id, email, password_hash, role from users where email = ?').get(email);
+   const validPassword = bcrypt.compareSync(password, user.password_hash);
+   if(!user && !validPassword) return res.status(404).json({message:'User not found'});
    
    const token = jwt.sign({
       id: user.id,
       email: user.email,
-      role: user.role
+      role: user.role   
    }, 
    SECRET_KEY,{expiresIn:'1h'})
    
@@ -33,4 +34,30 @@ exports.reqData = (req,res)=>{
        console.error(err);
        res.status(500).json({error: 'Failed to fetch users'})
    }   
+}
+
+exports.createUsers = async (req,res) =>{
+    console.log("Received body:", req.body)
+    const {first_name, last_name, username, phone_number, email, password} = req.body;
+    try{
+        const saltRounds = 10;
+        const hashedPassword = await bcrypt.hash(password, saltRounds);
+        const insertUser = db.prepare(`
+            insert into users (
+                first_name,
+                last_name,
+                username,
+                phone_number,
+                email,
+                password_hash
+            ) values(
+                ?, ?, ?, ?, ?, ?
+            )
+        `);
+        insertUser.run(first_name, last_name, username, phone_number, email, hashedPassword);
+        res.status(201).json({message:'User registered successfully'});
+    }catch(err){
+        console.error(err);
+        res.status(500).json({error: 'User registration failed'}) 
+    }
 }
