ea2d34363fb08c50ae8be559e3f30fbe92f568ba
John Philip Barnachia <jpbarnachia1234@gmail.com>
2025-05-07 16:18:46 +0800

    .

--- client/src/page/cashier-dashboard/cashier-dashboard.tsx
+++ client/src/page/cashier-dashboard/cashier-dashboard.tsx
@@ -81,7 +81,7 @@ function CashierDashboard() {
                 </div>
                 <Receipt />
             </div>
-        </SearchProvider>
+            </SearchProvider>
         </SelectedProductsProvider>
     );
 }
